08f9470f5279df4b72584ef8e8a1477d3539f322
julian <a.sleszynski@gmail.com>
2014-12-15 18:34:12 +0100

    Transfer baby

--- src/Chapter1/Ex8/Ex8_6.java
+++ src/Chapter1/Ex8/Ex8_6.java
@@ -1,17 +1,22 @@
 package Chapter1.Ex8;
 
+import java.io.UnsupportedEncodingException;
+
 /**
  * Created by julian on 13.12.14.
  */
 public class Ex8_6 {
-    public static void main(String[] args) {
-        String kosa = "123";
+    public static void main(String[] args)  throws UnsupportedEncodingException {
 
-        long b = 321;
-        int a = Integer.parseInt(kosa);
-
+        String a = "123";
+        long c = 321;
         int scorea = 0;
-        int scoreb = 0;
+        byte[] infoBin = a.getBytes("UTF-8");
+        for (byte b : infoBin) {
+            scorea += Character.getNumericValue(b);
+        }
+        System.out.println(scorea);
 
+        
     }
 }
